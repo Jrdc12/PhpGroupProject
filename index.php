b58c6e5878c69c154412c40317f9ff97cb187269
Jrdc12 <57374686+Jrdc12@users.noreply.github.com>
2021-12-10 16:16:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'functions.php';
 checkLogin();
 ?>
@@ -31,24 +32,13 @@ checkLogin();
 
 <div class="wrapper fadeInDown">
     <div id="formContent">
-        <!-- Tabs Titles -->
 
-        <!-- Icon -->
-        <!--<div class="fadeIn first">
-            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
-        </div>-->
-
-        <!-- Login Form -->
         <form method="post">
             <input type="text" id="login" class="fadeIn second" name="login" placeholder="login" value="admin">
             <input type="text" id="password" class="fadeIn third" name="password" placeholder="password" value="123456">
             <input type="submit" class="fadeIn fourth" value="Log In">
         </form>
 
-        <!-- Remind Passowrd -->
-        <div id="formFooter">
-            <a class="underlineHover" href="#">Forgot Password?</a>
-        </div>
 
     </div>
 </div>
