c9c975e59c8188b89a9e2bd0379386f9ab8a5e5a
Jrdc12 <57374686+Jrdc12@users.noreply.github.com>
2021-12-10 23:40:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'functions.php';
 checkLogin();
 ?>
@@ -15,18 +16,22 @@ checkLogin();
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="CSS1.css">
+    <style>
+
+
+
+
+
+
+
+    </style>
+
 </head>
 <body>
 
 
 <div class="wrapper fadeInDown">
     <div id="formContent">
-        <!-- Tabs Titles -->
-
-        <!-- Icon -->
-        <!--<div class="fadeIn first">
-            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
-        </div>-->
 
         <!-- Login Form -->
         <form method="post">
@@ -35,15 +40,22 @@ checkLogin();
             <input type="submit" class="fadeIn fourth" value="Log In">
         </form>
 
-        <!-- Remind Passowrd -->
-        <div id="formFooter">
-            <a class="underlineHover" href="#">Forgot Password?</a>
-        </div>
+
     </div>
 </div>
 
+
+
+
 <script src=https://my.gblearn.com/js/loadscript.js></script>
+
 </body>
+
+
+
+
+
+
 </html>
 
 <?php
